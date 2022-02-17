9452903407fc3b825fa038a3471d1c5a19f70034
Luciano <97047885+LucianoFerreiraRamos@users.noreply.github.com>
2022-02-17 15:00:29 -0300

    Aula04Poo1

---
+++ src/aula0poo1/Aula04Poo1.java
@@ -0,0 +1,16 @@
+package aula0poo1;
+
+public class Aula04Poo1 {
+	public static void main(String [] args ) {
+		Caneta c1 = new Caneta("NIC" , "Amarela " , 0.4f);
+		
+		c1.status();
+		System.out.println();
+		
+		Caneta c2 = new Caneta ("KKK", "Laranja" , 1.5f);
+		c2.status();
+
+		
+}
+	
+}
